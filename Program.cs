04a66d997775b5d49125ccba09ea42961e1d02a2
Khubaib Farooq <khubaibf@usf.edu>
2019-08-29 16:31:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first Console App");
+            Console.WriteLine("I am Learning how to publish to GitHub");
+            
+            // A change
+            
+        }
+    }
+}
